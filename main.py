cbd3ad9d02c82f04479f37ae949336eed9c36231
JT Raber <JMANN240raber@gmail.com>
2021-06-13 22:58:23 -0400

    friendlier path config

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def api_settings():
     if request.method == 'POST':
         with open('settings.json') as settings_file:
             settings = json.load(settings_file)
-        settings['filetypes'] = request.form
+        settings = request.json
         with open('settings.json', 'w') as settings_file:
             json.dump(settings, settings_file, indent=4, separators=(', ', ': '))
         return "200"
--- static/js/settings.js
+++ static/js/settings.js
@@ -2,13 +2,26 @@ const audio_color = document.querySelector('#audio-color');
 const video_color = document.querySelector('#video-color');
 const image_color = document.querySelector('#image-color');
 const text_color = document.querySelector('#text-color');
+const search_paths = document.querySelector('#search-paths');
+const client_upload_path = document.querySelector('#client-upload-path');
 
 $(document).on('click', '#save-btn', (e) => {
+    console.log(JSON.stringify(search_paths.value.slice(1, -1).split(',').map(x => x.trim().slice(1, -1))));
     $.ajax({
         type: 'POST',
         url: '/api/settings',
-        data: {audio: audio_color.value, video: video_color.value, image: image_color.value, text: text_color.value},
-        mimeType: 'json',
+        contentType: 'application/json',
+        dataType : 'json',
+        data: JSON.stringify({
+            filetypes: {
+                audio: audio_color.value, 
+                video: video_color.value, 
+                image: image_color.value, 
+                text: text_color.value
+            },
+            search_paths: search_paths.value.slice(1, -1).split(',').map(x => x.trim().slice(1, -1)),
+            client_upload_path: client_upload_path.value
+        }),
         success: () => {
             $(e.target).addClass('good-flash');
             setTimeout(() => {
